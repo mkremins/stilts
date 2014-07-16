c7fd6ffccc376a7af9d5b771c391e14631e14faa
Max Kreminski <maxkreminski@gmail.com>
2014-07-16 07:47:46 -0400

    Allow splitting of forms across multiple lines. Fixes #2.

--- src/stilts/repl.cljs
+++ src/stilts/repl.cljs
@@ -8,13 +8,20 @@
 
 (def ^:dynamic *repl-env* stilts/default-env)
 
+(def code-buffer (atom []))
+
 (defn eval-print! [interface line]
-  (try (let [form (rdr/read-string line)
+  (try (let [form (rdr/read-string (apply str (conj @code-buffer line)))
              [res env] (stilts/eval form *repl-env*)]
+         (.setPrompt interface "user=> ")
+         (reset! code-buffer [])
          (set! *repl-env* env)
          (prn res))
        (catch js/Error e
-         (println (str "Error: " (.-message e))))
+         (if (= (.-message e) "EOF while reading")
+           (do (.setPrompt interface "  #_=> ")
+               (swap! code-buffer conj (str line " ")))
+           (println (str "Error: " (.-message e)))))
        (finally
          (.prompt interface))))
 
