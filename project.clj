95a3a418f1ba322fc95646d690fa0b47a70a15b4
Max Kreminski <maxkreminski@gmail.com>
2014-07-18 09:05:21 -0400

    Add dependency: Medley 0.5.0

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2268"]]
+                 [org.clojure/clojurescript "0.0-2268"]
+                 [medley "0.5.0"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :cljsbuild {:builds [{:source-paths ["src"]
                         :compiler {:output-to "target/repl.js"
